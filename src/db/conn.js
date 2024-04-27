42fea14bbcb98abe68e586d230e9cd08bbe183d5
soban maruf <141897801+Sobanmaruf@users.noreply.github.com>
2024-04-27 15:35:10 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb+srv://saimkaskar1:l7TXntK1l6h8vKct@cluster0.nhzz0a4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://soban:XJvR1XEkAUQr4UOT@cluster0.aobwbkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {
         console.log(`Successfully Connected to Mongodb`);
     }).catch((e) => {
